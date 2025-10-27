dd75cf67ea6ede5ef487d9d771950a1dccb180e3
Urwa6 <115958392+Urwa6@users.noreply.github.com>
2025-10-27 02:54:23 -0900

    Define Board class fields for game setup
    Added fields for rows, columns, mine count, and grid.

--- src/se/team/minesweeper/Board.java
+++ src/se/team/minesweeper/Board.java
@@ -1,4 +1,15 @@
 package se.team.minesweeper;
+import java.util.Random;
 
+//Board represents entire game board with rows, column & cell
 public class Board {
+    //Number of rows
+    private final int rows;
+    //Number of columns
+    private final int cols;
+    //Number of mines
+    private final int mineCount;
+    //2-D array with all cells
+    private final Cell[][] grid;
+  
 }
