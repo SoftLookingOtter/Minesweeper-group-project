d8240a21e3219ed9a9b6b29e43715e20c2ec44fe
Sara Lindén <213674017+SoftLookingOtter@users.noreply.github.com>
2025-10-27 17:26:20 +0100

    feat: add Game class with basic setup and constructor

--- src/se/team/minesweeper/Game.java
+++ src/se/team/minesweeper/Game.java
@@ -1,4 +1,17 @@
 package se.team.minesweeper;
 
+//Game is responsible for the game's logic and order(game-loop)
 public class Game {
-}
+    //The game board
+    private final Board board;
+    //Player
+    private final Player player;
+    //True if game ends
+    private boolean gameOver;
+
+    //Creates a new game with the specified dimensions & number of mines
+    public Game(int rows, int cols, int mines) {
+        board = new Board(rows, cols, mines);
+        player = new Player();
+        gameOver = false;
+    }
