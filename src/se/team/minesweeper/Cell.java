5c2b01ba43011952427450b8e40f3e0e2f3a2ecb
OmarQasoma <o.qasoma@gmail.com>
2025-10-29 07:05:34 +0100

    Add Cell Logic

--- src/se/team/minesweeper/Cell.java
+++ src/se/team/minesweeper/Cell.java
@@ -1,5 +1,21 @@
 package se.team.minesweeper;
-// add Cell logic,
 
+//Cell represents a single square on the board
+//It knows if it is a mine, if it is revealed and how many mines are adjacent to it
 public class Cell {
+    //True if the box contains a mine
+    private boolean mine;
+    //True if the box is revealed
+    private boolean revealed;
+    //Number of mines around the box
+    private int adjacentMines;
+
+    //Contructing that sets the default values
+    public Cell() {
+        this.mine = false;
+        this.revealed = false;
+        this.adjacentMines = 0;
+    }
+
+
 }
